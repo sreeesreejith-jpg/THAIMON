1fea5c140c5d909198e30a6e7b1865b5b2e3b86e
Sreejith <sreee.sreejith@gmail.com>
2026-01-23 10:50:54 +0530

    chore: setup shared signing config and keystore script

---
+++ generate_keystore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Generate debug.keystore for shared signing
+cd android/app
+keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Debug,O=Android,C=US"
+echo "debug.keystore generated in android/app/"
